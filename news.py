1d861ec6a1865b54b4ce9e70b51032bb13d533d2
aprianditia <116142211+aprianditia@users.noreply.github.com>
2023-01-23 18:21:15 +0700

    Update news.py

--- news.py
+++ news.py
@@ -26,7 +26,7 @@ def send_news(context):
     news_list = get_latest_news()
     for news in news_list:
         message = f"Sumber: {news[0]}\nJudul: {news[1]}\nLink: {news[2]}\n"
-        context.bot.send_message(chat_id=update.message.chat_id, text=message)
+        context.bot.send_message(chat_id=SukamajuPetarunk, text=message)
 
 # Tambahkan fungsi send_news sebagai handler untuk perintah /news
 news_handler = CommandHandler('news', send_news)
