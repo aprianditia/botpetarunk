db7f2e0a4a2a5b504bc8697c3a84d607660abbf6
aprianditia <116142211+aprianditia@users.noreply.github.com>
2023-01-23 19:11:45 +0700

    Update news.py

--- news.py
+++ news.py
@@ -1,32 +1,16 @@
 import requests
-import json
-from telegram.ext import Updater, CommandHandler
+from bs4 import BeautifulSoup
 
-# Function to get current crypto price from Indodax
-def get_price(crypto):
-    url = "https://indodax.com/api/{}/ticker".format(crypto)
-    response = requests.get(url)
-    data = json.loads(response.text)
-    return data["ticker"]["last"]
+# Ambil halaman web akun Twitter yang ditentukan
+twitter_handle = "tier10k"
+url = f"https://twitter.com/{twitter_handle}"
+response = requests.get(url)
 
-# Function to check for price change and send notification
-def check_price(update, context):
-    crypto = "BTC"  # Example for Bitcoin
-    current_price = get_price(crypto)
-    if current_price > previous_price + (previous_price * 0.3):
-        message = "ALERT: {} price has increased by 30%! Current price: {}".format(crypto, current_price)
-    elif current_price < previous_price - (previous_price * 0.3):
-        message = "ALERT: {} price has decreased by 30%! Current price: {}".format(crypto, current_price)
-    else:
-        message = "No drastic change in {} price".format(crypto)
-    context.bot.send_message(chat_id=update.message.chat_id, text=message)
+# Parsing HTML
+soup = BeautifulSoup(response.text, "html.parser")
 
-# Telegram bot setup
-updater = Updater(token="YOUR_BOT_TOKEN", use_context=True)
-dispatcher = updater.dispatcher
+# Cari tweet terbaru
+tweet = soup.find("div", class_="js-tweet-text-container").text.strip()
 
-# Command handler for /checkprice command
-dispatcher.add_handler(CommandHandler("checkprice", check_price))
-
-# Start polling
-updater.start_polling()
+# Kirim tweet ke grup Telegram
+bot.send_message(chat_id=SukamajuPetarunk, text=tweet)
